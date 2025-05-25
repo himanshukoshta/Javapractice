b01e998211c00a53deff9ebcb9a0c56d8236cc55
himanshukoshta <koshtahimanshu89@gmail.com>
2025-05-25 11:02:32 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,25 @@
+
+class student {
+    String firstname="Sankalp";
+    String lastname="ki";
+    void fullname(){
+        System.out.println(firstname+" "+lastname);
+    }
+}
+class zzz{
+    public static void main(String[] args) {
+        student obj=new student();
+        obj.fullname();
+        yyy ob = new yyy();
+        ob.area();
+
+    }
+}
+class yyy{
+    float as=10;
+    float asss=(float)2.5;
+    void area()
+    {
+        System.out.println(as*asss);
+    }
+}
